f927e7083cab0d56bc8876f45817fcbc4f7a9a05
Alexander C. Schreyer <mail@alexschreyer.net>
2015-02-22 22:18:16 -0500

    Code cleanup

--- as_shapestoolbar.rb
+++ as_shapestoolbar.rb
@@ -1,7 +1,60 @@
-# Loader for as_shapestoolbar/as_shapestoolbar.rb
+=begin
+
+Copyright 2015, Alexander C. Schreyer
+All rights reserved
+
+THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE.
+
+License:        GPL (http://www.gnu.org/licenses/gpl.html)
+
+Author :        Alexander Schreyer, www.alexschreyer.net, mail@alexschreyer.net
+
+Website:        http://www.alexschreyer.net/projects/place-shapes-toolbar-extension-for-sketchup/
+
+Name :          Place Shapes Toolbar
+
+Version:        1.0
+
+Date :          2/2/2015
+
+Description :   A toolbar (and menu item) that lets you place several
+                primitives.
+                All are based on 1 ft in the US and 1 m everywhere else.
+
+Usage :         The easiest way is to show the "Place Shapes" toolbar and click
+                on the shape you want to place (or use the Draw menu's items.
+                Most shapes will attach themselves to the underlying geometry.
+                Use the move and stretch tool in combination with the solid
+                tools to work with them.
+                You can also always explode them to work with the underlying faces.
+
+History:        1.0 (2/2/2015):
+                - Initial release
+                - Added wedge, roof, vault
+                - Added cpoints to all spheres, etc.
+                - Rotated and scaled pyramid
+                - Sorted shapes better
+                TBD:
+                - Code cleanup
+
+Issues:         N/A
+
+TODO List:      - Preferences: Base unit (inch, mm,...) and scale, stickyness
+
+=end
+
+
+# =====================
+
 
 require 'sketchup'
 require 'extensions'
+
+
+# =====================
+
 
 as_shapestoolbar = SketchupExtension.new "Place Shapes Toolbar", "as_shapestoolbar/as_shapestoolbar.rb"
 as_shapestoolbar.copyright= 'Copyright 2015 Alexander C. Schreyer'
@@ -9,3 +62,6 @@ as_shapestoolbar.creator= 'Alexander C. Schreyer, www.alexschreyer.net'
 as_shapestoolbar.version = '1.0'
 as_shapestoolbar.description = "A toolbar (and menu item) that lets you place several primitives. All are based on 1 ft in the US and 1 m everywhere else."
 Sketchup.register_extension as_shapestoolbar, true
+
+
+# =====================
--- as_shapestoolbar/as_shapestoolbar.rb
+++ as_shapestoolbar/as_shapestoolbar.rb
@@ -1,54 +1,21 @@
-=begin
-
-Copyright 2015, Alexander C. Schreyer
-All rights reserved
-
-THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES,
-INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-FITNESS FOR A PARTICULAR PURPOSE.
-
-License:        GPL (http://www.gnu.org/licenses/gpl.html)
-
-Author :        Alexander Schreyer, www.alexschreyer.net, mail@alexschreyer.net
-
-Website:        http://www.alexschreyer.net/projects/place-shapes-toolbar-extension-for-sketchup/
-
-Name :          Place Shapes Toolbar
-
-Version:        1.0
-
-Date :          2/2/2015
-
-Description :   A toolbar (and menu item) that lets you place several
-                primitives.
-                All are based on 1 ft in the US and 1 m everywhere else.
-                
-Usage :         The easiest way is to show the "Place Shapes" toolbar and click
-                on the shape you want to place (or use the Draw menu's items.
-                Most shapes will attach themselves to the underlying geometry.
-                Use the move and stretch tool in combination with the solid
-                tools to work with them.
-                You can also always explode them to work with the underlying faces.
-
-History:        1.0 (2/2/2015):
-                - Initial release
-                - Added wedge, roof, vault
-                - Added cpoints to all spheres, etc.
-                - Rotated and scaled pyramid
-                - Sorted shapes better
-                
-Issues:         N/A              
-                
-TODO List:      N/A
-
-=end
+# =====================
+# Main file for Place Shapes Toolbar
+# =====================
 
 
 require 'sketchup'
 
+
+# =====================
+
+
 module AS_Extensions
 
   module AS_Shapestoolbar
+
+
+    # =====================
+
   
     # General variables
     @name = "Place Shapes"
@@ -60,6 +27,10 @@ module AS_Extensions
 
     # @skps = Dir.entries(@sfolder)
     # UI.messagebox @skps
+
+
+    # =====================
+
     
     def self.place_me(name)
       
@@ -71,6 +42,10 @@ module AS_Extensions
       mod.place_component c, true
     
     end
+
+
+    # =====================
+
     
     # Load extension at startup and add menu items
     if !file_loaded?(__FILE__)
@@ -104,7 +79,14 @@ module AS_Extensions
       file_loaded(__FILE__)
     
     end # if  
+
+
+    # =====================
+
   
   end # AS_Shapestoolbar
   
 end # AS_Extensions
+
+
+# =====================
